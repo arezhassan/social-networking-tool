c656be8811e7a5fbc54f5f3dadbc486827c4c231
A.MAJID <103788155+Majid9287@users.noreply.github.com>
2022-06-28 15:12:10 +0500

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,583 @@
+#include<iostream>
+using namespace std;
+#include <iostream>
+using namespace std;
+
+class Node_Avl {
+public:
+    string key;
+    Node_Avl* left;
+    Node_Avl* right;
+    int height;
+};
+
+string max(string a, string b);
+
+// Calculate height
+int height(Node_Avl* N) {
+    if (N == NULL)
+        return 0;
+    return N->height;
+}
+
+string max(string a, string b) {
+    return (a > b) ? a : b;
+}
+
+// New node creation
+Node_Avl* newNode(string key) {
+    Node_Avl* node = new Node_Avl();
+    node->key = key;
+    node->left = NULL;
+    node->right = NULL;
+    node->height = 1;
+    return (node);
+}
+
+// Rotate right
+Node_Avl* rightRotate(Node_Avl* y) {
+    Node_Avl* x = y->left;
+    Node_Avl* T2 = x->right;
+    x->right = y;
+    y->left = T2;
+    y->height = max(height(y->left),
+        height(y->right)) +
+        1;
+    x->height = max(height(x->left),
+        height(x->right)) +
+        1;
+    return x;
+}
+
+// Rotate left
+Node_Avl* leftRotate(Node_Avl* x) {
+    Node_Avl* y = x->right;
+    Node_Avl* T2 = y->left;
+    y->left = x;
+    x->right = T2;
+    x->height = max(height(x->left),
+        height(x->right)) +
+        1;
+    y->height = max(height(y->left),
+        height(y->right)) +
+        1;
+    return y;
+}
+
+// Get the balance factor of each node
+int getBalanceFactor(Node_Avl* N) {
+    if (N == NULL)
+        return 0;
+    return height(N->left) -
+        height(N->right);
+}
+
+// Insert a node
+Node_Avl* insertNode(Node_Avl* node, string key) {
+    // Find the correct postion and insert the node
+    if (node == NULL)
+        return (newNode(key));
+    if (key < node->key)
+        node->left = insertNode(node->left, key);
+    else if (key > node->key)
+        node->right = insertNode(node->right, key);
+    else
+        return node;
+
+    // Update the balance factor of each node and
+    // balance the tree
+    node->height = 1 + max(height(node->left),
+        height(node->right));
+    int balanceFactor = getBalanceFactor(node);
+    if (balanceFactor > 1) {
+        if (key < node->left->key) {
+            return rightRotate(node);
+        }
+        else if (key > node->left->key) {
+            node->left = leftRotate(node->left);
+            return rightRotate(node);
+        }
+    }
+    if (balanceFactor < -1) {
+        if (key > node->right->key) {
+            return leftRotate(node);
+        }
+        else if (key < node->right->key) {
+            node->right = rightRotate(node->right);
+            return leftRotate(node);
+        }
+    }
+    return node;
+}
+
+// Node with minimum value
+Node_Avl* nodeWithMimumValue(Node_Avl* node) {
+    Node_Avl* current = node;
+    while (current->left != NULL)
+        current = current->left;
+    return current;
+}
+
+// Delete a node
+Node_Avl* deleteNode(Node_Avl* root, string key) {
+    // Find the node and delete it
+    if (root == NULL)
+        return root;
+    if (key < root->key)
+        root->left = deleteNode(root->left, key);
+    else if (key > root->key)
+        root->right = deleteNode(root->right, key);
+    else {
+        if ((root->left == NULL) ||
+            (root->right == NULL)) {
+            Node_Avl* temp = root->left ? root->left : root->right;
+            if (temp == NULL) {
+                temp = root;
+                root = NULL;
+            }
+            else
+                *root = *temp;
+            free(temp);
+        }
+        else {
+            Node_Avl* temp = nodeWithMimumValue(root->right);
+            root->key = temp->key;
+            root->right = deleteNode(root->right,
+                temp->key);
+        }
+    }
+
+    if (root == NULL)
+        return root;
+
+    // Update the balance factor of each node and
+    // balance the tree
+    root->height = 1 + max(height(root->left),
+        height(root->right));
+    int balanceFactor = getBalanceFactor(root);
+    if (balanceFactor > 1) {
+        if (getBalanceFactor(root->left) >= 0) {
+            return rightRotate(root);
+        }
+        else {
+            root->left = leftRotate(root->left);
+            return rightRotate(root);
+        }
+    }
+    if (balanceFactor < -1) {
+        if (getBalanceFactor(root->right) <= 0) {
+            return leftRotate(root);
+        }
+        else {
+            root->right = rightRotate(root->right);
+            return leftRotate(root);
+        }
+    }
+    return root;
+}
+
+// Print the tree
+void printTree(Node_Avl* root) {
+    if (root != nullptr) {
+        printTree(root->left);
+        cout << root->key << " , ";
+        printTree(root->right);
+    }
+}
+class Node
+{
+public:
+    float data;
+    Node* next;
+};
+
+template<class T>
+class node
+{
+private:
+
+    T  data;
+    node* nextnode;
+public:
+    node() {
+
+        this->data = 0;
+
+        this->nextnode = NULL;
+    }
+    node(T data) {
+
+        this->data = data;
+
+        this->nextnode = NULL;
+    }
+    void setdata(T marks) {
+        this->data = marks;
+    }
+    void setnextnode(node* nextnode) {
+        this->nextnode = nextnode;
+    }
+    T getdata() {
+        return this->data;
+    }
+    node* getnextnode() {
+        return this->nextnode;
+    }
+};
+template<class T>
+class LINKLIST
+{
+private:
+    node<T>* head;
+    node<T>* tail;
+public:
+    LINKLIST<T>()
+    {
+        head = NULL;
+        tail = NULL;
+    }
+    void enque(T data)
+    {
+
+        node< T>* t = new node<T>(data);
+        if (head == NULL)
+        {
+            head = t;
+            tail = t;
+        }
+        else
+        {
+            tail->setnextnode(t);
+            tail = t;
+
+        }
+
+
+    }
+    void display()
+    {
+        if (head == NULL)
+        {
+            cout << "List is empty" << endl;
+        }
+        else
+        {
+            node<T>* temp = head;
+            while (temp != NULL)
+            {
+                cout << temp->getdata() << endl;
+                temp = temp->getnextnode();
+            }
+        }
+    }
+    void deleteFromStart()
+    {
+        if (head == NULL)
+        {
+            cout << "List is empty" << endl;
+        }
+        else
+        {
+            node<T>* temp = head;
+
+            if (head == tail)
+            {
+                head = tail = NULL;
+                delete temp;
+            }
+            else
+            {
+                head = head->getnextnode();
+                delete temp;
+            }
+        }
+    }
+
+
+    void deleteData(T data)
+    {
+        if (head == NULL)
+        {
+            cout << "List is empty" << endl;
+        }
+        else
+        {
+            node<T>* temp = head;
+
+            if (head->getdata() == data)
+            {
+                deleteFromStart();
+            }
+            else if (tail->getdata() == data)
+            {
+                deleteFromEnd();
+            }
+            else
+            {
+                node<T>* prev = head;
+                node<T>* temp = prev->getnextnode();
+
+                bool found = false;
+
+                while (temp->getnextnode() != NULL)
+                {
+                    if (temp->getdata() == data)
+                    {
+                        prev->setnextnode(temp->getnextnode());
+                        delete temp;
+                        found = true;
+                        break;
+                    }
+
+                    prev = temp;
+                    temp = temp->getnextnode();
+                }
+
+                if (found == false)
+                {
+                    cout << "Data not found" << endl;
+                }
+                else
+                {
+                    cout << "Data deleted successfully" << endl;
+                }
+            }
+        }
+    }
+
+
+
+    void deleteFromEnd()
+    {
+        if (head == NULL)
+        {
+            cout << "List is empty" << endl;
+        }
+        else
+        {
+            node<T>* temp = head;
+
+            if (head == tail)
+            {
+                head = tail = NULL;
+                delete temp;
+            }
+            else
+            {
+                while (temp->getnextnode() != tail)
+                {
+                    temp = temp->getnextnode();
+                }
+                tail = temp;
+                temp = temp->getnextnode();
+                delete temp;
+                tail->setnextnode(NULL);
+            }
+        }
+    }
+    node<T>* getHead() {
+        return head;
+    }
+    void update_element(T d, T update_ele)
+    {
+        int count = 0;
+
+        node<T>* temp = head;
+        while (temp != NULL) // Start traversing from head node
+        {
+            if (temp->getdata() == d)
+            {
+
+                temp->setdata(update_ele);// change the element in the list
+            }
+            else
+            {
+                count = count + 1;
+                temp = temp->getnext();
+            }
+        }
+    }
+    void Contactdelet(string n)
+    {
+        if (head == NULL)
+        {
+            cout << "List is empty" << endl;
+        }
+        else
+        {
+            node<T>* temp = head;
+
+            if (head->getdata().name == n || head->getdata().cell_phone == n)
+            {
+                deleteFromStart();
+            }
+            else if (tail->getdata().name == n || tail->getdata().cell_phone == n)
+            {
+                deleteFromEnd();
+            }
+            else
+            {
+                node<T>* prev = head;
+                node<T>* temp = prev->getnextnode();
+
+                bool found = false;
+
+                while (temp->getnextnode() != NULL)
+                {
+                    if (temp->getdata().name == n || temp->getdata().cell_phone == n)
+                    {
+                        prev->setnextnode(temp->getnextnode());
+                        delete temp;
+                        found = true;
+                        break;
+                    }
+
+                    prev = temp;
+                    temp = temp->getnextnode();
+                }
+
+                if (found == false)
+                {
+                    cout << "Data not found" << endl;
+                }
+                else
+                {
+                    cout << "Data deleted successfully" << endl;
+                }
+            }
+        }
+    }
+    void eventprint()
+    {
+        if (head == NULL)
+        {
+            cout << "List is empty" << endl;
+        }
+        else
+        {
+
+            node<T>* temp = head;
+            while (temp != NULL)
+            {
+
+                cout << temp->getdata().event_name << "   ";
+                cout << temp->getdata().date <<" ";
+                cout << temp->getdata().month <<" ";
+                cout << temp->getdata().year << endl;
+
+                temp = temp->getnextnode();
+            }
+        }
+    }
+    T search(string n, bool& check)
+    {
+        if (head == NULL)
+        {
+            cout << "List is empty" << endl;
+        }
+        else
+        {
+
+            node<T>* temp = head;
+            while (temp != NULL)
+            {
+
+                if (temp->getdata().name == n || temp->getdata().cell_phone == n) {
+                    check = true;
+                    return temp->getdata();
+                }
+                temp = temp->getnextnode();
+            }
+        }
+        check = false;
+        return head->getdata();
+
+    }
+
+
+};
+//std::string months[] = { "Jan", "Feb", "Mar", "Apr", "May", "Jun",
+//                        "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" };
+//std::string days[] = { "Sun", "Mon", "Tue", "Wed", "Thu", "Fri",
+//                      "Sat" };
+
+//class Date {
+//
+//public:
+//    std::string month;
+//    std::string day;
+//    int date;
+//    int year;
+//    // Public Members
+//public:
+//    // Default Constructor
+//    Date() {
+//        const int BASE_YEAR = 2022;
+//        time_t timer;
+//        tm* time;
+//        std::time(&timer);
+//        time = localtime(&timer);
+//        date = time->tm_mday;
+//        month = months[time->tm_mon];
+//        day = days[time->tm_wday];
+//        year = time->tm_year + BASE_YEAR;
+//    }
+//    void printDate(void) {
+//        std::cout << "Current date "
+//            << this->month << " " << this->day << " "
+//            << this->date << " " << this->year;
+//    }
+//    // Destructor
+//    ~Date() {}
+//};
+class event {
+public:
+    string event_name;
+    int month;
+    int date;
+    int year;
+   
+};
+class students {
+    LINKLIST <string> timeline;
+    LINKLIST <string >friends;
+    LINKLIST < event> events;
+public:
+    void Settimeline(string data) {
+        timeline.enque(data);
+    }
+    void setfriends(string data) {
+        friends.enque(data);
+    }
+    void setevents(event e) {
+        events.enque(e);
+    }
+   void getfriends() {
+       friends.display();
+    }
+    void getevents() {
+      //  events.display();
+    }
+    void gettimeline() {
+        timeline.display();
+    }
+};
+
+
+
+int main() {
+
+   /* Node* root = NULL;
+    root = insertNode(root, "abdul");
+    root = insertNode(root, "arez");
+    root = insertNode(root, "basit");
+    root = insertNode(root, "kis");
+    root = insertNode(root, "mis");
+    root = insertNode(root, "chis");
+    root = insertNode(root,"ll");
+    root = insertNode(root, "y");
+    printTree(root);
+    root = deleteNode(root, "y");
+    cout << "\nAfter deleting " << endl;
+    printTree(root);*/
+}
